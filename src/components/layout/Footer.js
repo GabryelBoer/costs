6b769408f23bf71d3d5f3f22db22c751945886a9
GabryelBoer <107651799+GabryelBoer@users.noreply.github.com>
2023-02-08 12:04:16 -0300

    costs

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,18 @@
+import {FaFacebook, FaInstagram, FaLinkedin} from 'react-icons/fa'
+
+import styles from './Footer.module.css'
+
+function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li><FaFacebook /></li>
+        <li><FaInstagram /></li>
+        <li><FaLinkedin /></li>
+      </ul>
+      <p className={styles.copy_right}><span>Costs</span> &copy; 2023</p>
+    </footer>
+  )
+}
+
+export default Footer
