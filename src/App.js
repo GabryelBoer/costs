f5d5312cf6b1bafb232569a4fc11868c88e5b176
GabryelBoer <107651799+GabryelBoer@users.noreply.github.com>
2023-02-18 21:07:33 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Container from './components/layout/Container';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
-
 function App() {
   return (
     <Router>
